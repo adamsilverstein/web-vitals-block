15a9a70109d5dfdb356565473324cbbfb965e256
Adam Silverstein <adamjs@google.com>
2024-01-24 16:50:21 -0700

    update dependencies

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'caf067de5c087b719bd157067340b7b0');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'd385eb6da1195ce2f98b03742128f7e7');
